03cd07a6195ee205cfd6fdcdf3ae4e3b8e867b99
VincentRJ <147723871+VincentRJ@users.noreply.github.com>
2026-02-12 14:47:25 +0700

    Pertemuan-3

---
+++ Pertemuan 3/p022WorkingVariabel.java
@@ -0,0 +1,19 @@
+public class p022WorkingVariabel {
+  public static void main(String[] args) {
+    // Deklarasi dan inisialisasi variabel
+    int angka = 10;
+
+    // Menampilkan nilai awal variabel
+    System.out.println("Nilai angka awal : " + angka);
+
+    // Mengubah nilai variabel
+    angka = 25;
+
+    // Menampilkan nilai setelah perubahan
+    System.out.println("Nilai angka setelah diubah : " + angka);
+  }
+}
+
+// Nama : Vincent Rian Jonathan
+// NIM : D1041241075
+// Hari/Tanggal : Kamis/12-02-2026
