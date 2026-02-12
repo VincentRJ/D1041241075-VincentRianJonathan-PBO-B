52a9a57ca6487c345d27991973a2dd0c5732bea2
VincentRJ <147723871+VincentRJ@users.noreply.github.com>
2026-02-12 14:47:13 +0700

    Pertemuan-1

---
+++ Pertemuan 1/VincentRianJonathan_D1041241075_Semester4.java
@@ -0,0 +1,12 @@
+public class VincentRianJonathan_D1041241075_Semester4 {
+    public static void main(String[] args) {
+      System.out.println("Nama : Vincent Rian Jonathan");
+      System.out.println("NIM : D1041241075");
+      System.out.println("Semester : 4");
+      System.out.println("Prodi : Informatika");
+    }
+}
+
+// Nama : Vincent Rian Jonathan
+// NIM : D1041241075
+// Hari/Tanggal : Kamis,05-02-2026
---
+++ Pertemuan 1/kodepertama.java
@@ -0,0 +1,10 @@
+public class kodepertama {
+    public static void main(String[] args) {
+        System.out.println("Halo, Pemograman Berorientasi Objek");
+        System.out.println("Ini adalah program Java pertama saya");
+    }
+}
+
+// Nama : Vincent Rian Jonathan
+// NIM : D1041241075
+// Hari/Tanggal : Kamis,05-02-2026
