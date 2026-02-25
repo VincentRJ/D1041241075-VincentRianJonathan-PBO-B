5efaf05eda3a98e9572665e196c6c9f1a8bdb62a
VincentRJ <147723871+VincentRJ@users.noreply.github.com>
2026-02-25 20:54:58 +0700

    Menambahkan Tugas 1 Pertemuan 7

---
+++ Pertemuan7/Pertemuan7/LatihanDeretGanjil.java
@@ -0,0 +1,21 @@
+package Pertemuan7;
+
+public class LatihanDeretGanjil {
+  public static void main(String[] args) {
+    //Menginisialisasi nilai inputan awal dengan mendeklarasikan variabel integer bernilai 1
+    int angka = 1; 
+    System.out.println("=== Bilangan Ganjil 1-15 ===");
+
+    // Kondisi perulangan while dengan syarat nilai angka dibawah atau sama dengan 15 bernilai true jika false maka perluangannya tidak akan dijalankan
+    //mencetak hasilnya agar sebaris dengan dipisahkan spasi
+    //karena kita telah menginisialisasi angka dengan 1 maka tinggal ditambahkan didalam perulangan bernilai 2 agar tetap ganjil
+    while(angka <= 15){
+      System.out.print(angka + " "); 
+      angka+= 2; 
+    }
+  }
+}
+
+// Nama : Vincent Rian Jonathan
+// NIM : D1041241075
+// Hari/Tanggal : Senin/25-02-2026
