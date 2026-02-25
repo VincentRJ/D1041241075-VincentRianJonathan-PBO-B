e777b4f851adec6fb58bc5f48ddb173b4298f9d9
VincentRJ <147723871+VincentRJ@users.noreply.github.com>
2026-02-25 20:56:03 +0700

    Menambahkan Tugas 3 Pertemuan 7

---
+++ Pertemuan7/Pertemuan7/LatihanTabelPerkalian.java
@@ -0,0 +1,17 @@
+package Pertemuan7;
+
+public class LatihanTabelPerkalian {
+  public static void main(String[] args) {
+    int angka = 7; // Menginisialisasi angka yang akan dikalikan
+    System.out.println("=== TABEL PERKALIAN 7 ===");
+
+    // Perulangan untuk mengalikan nilai 7 dari range 1-12
+    for(int perulangan = 1; perulangan <=12; perulangan++){
+      System.out.println("7 x " + perulangan + " = " + angka * perulangan); 
+    }
+  }
+}
+
+// Nama : Vincent Rian Jonathan
+// NIM : D1041241075
+// Hari/Tanggal : Senin/25-02-2026
