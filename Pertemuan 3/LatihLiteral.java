170389759e07c744a39f557ff474e33a9ef85196
VincentRJ <147723871+VincentRJ@users.noreply.github.com>
2026-03-05 21:08:26 +0700

    Menambahkan Latihan 3 Pertemuan 3

---
+++ Pertemuan 3/LatihLiteral.java
@@ -0,0 +1,21 @@
+public class LatihLiteral {
+  public static void main(String[] args) {
+    byte tipeByte = 127;
+    short tipeShort = 2026;
+    int tipeInteger = 275000000;
+    long tipeLong = 8000000000L; 
+    float tipeFloat = 3.14f;
+    double tipeDouble = 2.718281828;
+    char tipeChar = 'B';
+    boolean isTipeBoolean = true;
+    System.out.println("=== DEMONSTRASI LITERAL TIPE DATA ===");
+    System.out.println("Tipe byte : " + tipeByte);
+    System.out.println("Tipe short : " + tipeShort);
+    System.out.println("Tipe int : " + tipeInteger);
+    System.out.println("Tipe long : " + tipeLong);
+    System.out.println("Tipe float : " + tipeFloat);
+    System.out.println("Tipe double : " + tipeDouble);
+    System.out.println("Tipe char : " + tipeChar);
+    System.out.println("Tipe boolean : " + isTipeBoolean);
+  }
+}
