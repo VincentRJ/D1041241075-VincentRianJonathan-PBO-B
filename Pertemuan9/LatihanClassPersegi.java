273a97e9fe88e266724f800e7695f93c52a68b03
VincentRJ <147723871+VincentRJ@users.noreply.github.com>
2026-03-06 21:17:16 +0700

    Menambahkan Tugas 3 Pertemuan 9

---
+++ Pertemuan9/LatihanClassPersegi.java
@@ -0,0 +1,46 @@
+// Inisialisasi class
+class Persegi{
+  // Deklarasi atribut sesuai UML
+  int sisi;
+  int luas;
+  int keliling;
+
+  // Method untuk menghitung luas
+  public void hitungLuas(){
+    luas = sisi * sisi;
+  }
+
+  // Method untuk menghitung keliling
+  public void hitungKeliling(){
+    keliling = 4 * sisi;
+  }
+
+  // Method untuk menampilkan informasi terkait persegi sisi,luas, dan keliling
+  public void hitungHasil(){
+    System.out.println("=== PERSEGI ===");
+    System.out.println("");
+    System.out.println("Sisi     : " + sisi + " cm");
+    System.out.println("");
+    System.out.println("Luas     : " + luas + " cm2²");
+    System.out.println("");
+    System.out.println("Keliling : " + keliling + " cm");
+    System.out.println("");
+  }
+}
+
+public class LatihanClassPersegi {
+  public static void main(String[] args) {
+    Persegi persegi1 = new Persegi(); //Deklarasi dan inisialisasi Objek
+    persegi1.sisi = 5; //Menginisialisai nilai sisi
+
+    // Memanggil method untuk menghitung luas dan persegi
+    persegi1.hitungLuas();
+    persegi1.hitungKeliling();
+    // Menampilkan hasil informasi terkait persegi sisi,luas, dan keliling
+    persegi1.hitungHasil();
+  }
+}
+
+// Nama : Vincent Rian Jonathan
+// NIM : D1041241075
+// Hari/Tanggal : Jumat/6 Maret 2026
