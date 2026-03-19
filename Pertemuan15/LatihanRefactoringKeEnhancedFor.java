b55a9f41b661c46ddbcba7287e72c130a05e4ebb
VincentRJ <147723871+VincentRJ@users.noreply.github.com>
2026-03-19 20:17:30 +0700

    Menambahkan Latihan 1 Pertemuan 15

---
+++ Pertemuan15/LatihanRefactoringKeEnhancedFor.java
@@ -0,0 +1,16 @@
+package Pertemuan15;
+
+public class LatihanRefactoringKeEnhancedFor {
+  public static void main(String[] args) {
+    // Mendeklarasi array dan menginisialisasi atributnya dengan nama buah
+    String[] buah = {"Apel", "Jeruk", "Mangga", "Anggur"};
+    // Refactored dengan enhanced for untuk menampilkan isi array 
+    for (String b : buah) {
+      System.out.println("Buah: " + b);
+    }
+  }
+}
+
+// Nama : Vincent Rian Jonathan
+// NIM : D1041241075
+// Hari/Tanggal : Rabu/18 Maret 2026
