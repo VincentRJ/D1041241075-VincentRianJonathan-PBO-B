378db91c41ff43f55001141fc0b3e1b01ceb71f7
VincentRJ <147723871+VincentRJ@users.noreply.github.com>
2026-03-19 20:02:34 +0700

    Menambahkan Latihan 2 Pertemuan 13

---
+++ Pertemuan13/LatihanArrayNama.java
@@ -0,0 +1,18 @@
+package Pertemuan13;
+
+public class LatihanArrayNama {
+public static void main(String[] args) {
+    // Deklarasi Array dan inisialisasi atributnya berupa sekumpulan nama
+    String [] nama = {"Andi","Budi","Citra","Deni","Eka"};  
+    System.out.println("=== DAFTAR NAMA ===");
+
+//   For loop untuk mencetak nama dalam array dan membuat penomorannya.
+    for (int iterasi = 0; iterasi < nama.length; iterasi++) {
+        System.err.println((iterasi + 1) + ". " + nama[iterasi]);
+    }
+}
+}
+
+// Nama : Vincent Rian Jonathan
+// NIM : D1041241075
+// Hari/Tanggal : Rabu/18 Maret 2026
